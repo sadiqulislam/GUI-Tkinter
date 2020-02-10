bd539e4c3a9daa3aef327750495b7d06acc9b7bf
Shishir <sishishir2015@gmail.com>
2020-02-11 00:14:30 +0600

    Frame

---
+++ Frame.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+
+frame1 = Frame(root)
+frame1.pack()
+
+frame2 = Frame(root)
+frame2.pack(side=BOTTOM)
+
+button1 = Button(frame1,text="Click Here",fg="Green")
+button1.pack()
+
+button2 = Button(frame2,text="Click",fg="Red")
+button2.pack()
+
+root.mainloop()
