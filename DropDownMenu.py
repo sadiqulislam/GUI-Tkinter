c633168942d7a3242c222124b16b5c2968ee0eb1
Shishir <sishishir2015@gmail.com>
2020-02-11 20:52:26 +0600

    Drop Down Menu and separator

--- DropDownMenu.py
+++ DropDownMenu.py
@@ -17,4 +17,14 @@ submenu = Menu(mymenu)
 mymenu.add_cascade(label="File", menu=submenu)
 submenu.add_command(label="Open Project", command=openproject)
 submenu.add_command(label="Save", command=save)
+
+#Separator
+
+submenu.add_separator()
+submenu.add_command(label="Settings", command=openproject)
+submenu.add_command(label="General", command=save)
+
+
+
+
 root.mainloop()
