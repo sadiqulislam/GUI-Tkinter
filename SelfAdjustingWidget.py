0f12d911bc6efe092b5f7189617a9907a8026949
Shishir <sishishir2015@gmail.com>
2020-02-11 18:51:11 +0600

    Self Adjusting Widget

--- SelfAdjustingWidget.py
+++ SelfAdjustingWidget.py
@@ -6,6 +6,6 @@ label1 = Label(root, text="First Name", bg="Red", fg="Green")
 label2 = Label(root, text="Last Name", bg="Red", fg="Green")
 
 label1.pack(fill=X)
-label2.pack(fill=X)
+label2.pack(side=LEFT, fill=Y)
 
 root.mainloop()
